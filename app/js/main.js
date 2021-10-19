c7fd870770ee01dfe97440232a96d232908080ab
SvetlanaRyzhova98 <sweta300498@yandex.ru>
2021-10-19 14:41:07 +0300

    верстка двух секций слайдеров

--- app/js/main.js
+++ app/js/main.js
@@ -6,10 +6,10 @@ $(function () {
       '<img class="slider-arrows  slider-arrows__left" src="img/arrows-left.svg" alt="">',
     nextArrow:
       '<img class="slider-arrows  slider-arrows__right" src="img/arrows-right.svg" alt="">',
-    asNavFor: ".slider-dots",
+    asNavFor: ".slider-dotshead",
   });
 
-  $(".slider-dots").slick({
+  $(".slider-dotshead").slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: ".header__slider",
@@ -17,6 +17,23 @@ $(function () {
   $(".surf-slider").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
+    prevArrow:
+      '<img class="slider-arrows  slider-arrows__left" src="img/arrows-left.svg" alt="">',
+    nextArrow:
+      '<img class="slider-arrows  slider-arrows__right" src="img/arrows-right.svg" alt="">',
+    asNavFor: ".slider-map",
+  });
+
+  $(".slider-map").slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: ".surf-slider",
+    focusOnSelect: true,
+  });
+  $(".holder__slider").slick({
+    infinite: true,
+    fade: true,
     prevArrow:
       '<img class="slider-arrows  slider-arrows__left" src="img/arrows-left.svg" alt="">',
     nextArrow:
