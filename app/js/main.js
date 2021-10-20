9e50bcc0ad8e57cab44bcb44903da58ecdfd51b3
SvetlanaRyzhova98 <sweta300498@yandex.ru>
2021-10-20 17:43:26 +0300

    Верстка блока shop

--- app/js/main.js
+++ app/js/main.js
@@ -39,4 +39,54 @@ $(function () {
     nextArrow:
       '<img class="slider-arrows  slider-arrows__right" src="img/arrows-right.svg" alt="">',
   });
+
+  $(
+    '<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/Plus.svg" alt="plus"></div><div class="quantity-button quantity-down"><img src="img/Minus.svg" alt="minus"></div></div>'
+  ).insertAfter(".quantity input");
+  $(".quantity").each(function () {
+    var spinner = $(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find(".quantity-up"),
+      btnDown = spinner.find(".quantity-down"),
+      min = input.attr("min"),
+      max = input.attr("max");
+
+    btnUp.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue >= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+    btnDown.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+  });
+
+  $(".quantity-button").on("click", function () {
+    var parents = $(this).parents(".holder-slider__info");
+    let summ =
+      $(".summ", parents).data("nights") * $(".nights", parents).val() +
+      $(".summ", parents).data("guests") * $(".guests", parents).val();
+    $(".summ", parents).html("$" + summ);
+  });
+
+  $(".quantity").each(function () {
+    var parents = $(this).parents(".holder-slider__info");
+    let summ =
+      $(".summ", parents).data("nights") * $(".nights", parents).val() +
+      $(".summ", parents).data("guests") * $(".guests", parents).val();
+    $(".summ", parents).html("$" + summ);
+  });
 });
